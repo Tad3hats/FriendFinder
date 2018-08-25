14237f969e90c134f8ae0e1bb6791d5de44e39db
Tad <tad.3hatsmedia@gmail.com>
2018-08-25 00:20:51 -0500

    first commit

---
+++ app/data/friends.js
@@ -0,0 +1,24 @@
+var friends = [
+
+    {
+        "name": "Ahmed",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [5, 1, 4, 4, 5, 1, 2, 6, 7, 9]
+
+    },
+    {
+        "name": "Jim",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [5, 1, 4, 4, 5, 1, 2, 6, 7, 9]
+
+    },
+    {
+        "name": "Max",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [5, 1, 4, 4, 5, 1, 2, 6, 7, 9]
+
+    }
+]
+
+module.exports = friends;
+
