bf33683c1d83e643649b449292026095d30bf06a
Tad <tad.3hatsmedia@gmail.com>
2018-08-25 01:03:25 -0500

    still working on it

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,7 +2,7 @@ var friends = [
 
     {
         "name": "Ahmed",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "photo": "https://tinder.com/static/build/2bb3d19a06441920944637abb1b98d67.jpg",
         "scores": [5, 1, 4, 4, 5, 1, 2, 6, 7, 9]
 
     },
@@ -13,11 +13,17 @@ var friends = [
 
     },
     {
-        "name": "Max",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "name": "Donnie",
+        "photo": "https://cdn.vox-cdn.com/thumbor/CgeN3iG-iKDzJGG_EGETa0C9A8g=/0x0:3000x2227/1200x800/filters:focal(907x634:1387x1114)/cdn.vox-cdn.com/uploads/chorus_image/image/59377179/944308308.jpg.0.jpg",
         "scores": [5, 1, 4, 4, 5, 1, 2, 6, 7, 9]
 
-    }
+    },
+    {
+        "name": "Jill",
+        "photo": "https://tinder.com/static/build/fcf9fe442c7cfd80da6433af784d59d9.jpg",
+        "scores": [5, 1, 4, 4, 5, 1, 2, 6, 7, 9]
+    },
+    
 ]
 
 module.exports = friends;
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -6,6 +6,7 @@ module.exports = function (app) {
         res.sendFile(path.join(__dirname, "../public/survey.html"))
     })
 
+    //any wierd routes get sent to the homepage
     app.get("*", function(req, res) {
         res.sendFile(path.join(__dirname, "../public/home.html"))
     })
