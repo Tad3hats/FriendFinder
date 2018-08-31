c197bc36159cc31cebe17673fa85be76145e4c24
Tad <tad.3hatsmedia@gmail.com>
2018-08-30 21:25:51 -0500

    complete routes

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,27 +3,27 @@ var friends = [
     {
         "name": "Ahmed",
         "photo": "https://tinder.com/static/build/2bb3d19a06441920944637abb1b98d67.jpg",
-        "scores": [5, 1, 4, 4, 5, 1, 2, 6, 7, 9]
+        "scores": [5, 5, 5, 5, 2, 5, 3, 5, 5, 5]
 
     },
     {
         "name": "Jim",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        "scores": [5, 1, 4, 4, 5, 1, 2, 6, 7, 9]
+        "photo": "https://s1.favim.com/orig/10/jim-halpert-john-krasinski-the-office-Favim.com-173661.jpg",
+        "scores": [1, 1, 1, 5, 1, 1, 2, 1, 3, 1]
 
     },
     {
         "name": "Donnie",
         "photo": "https://cdn.vox-cdn.com/thumbor/CgeN3iG-iKDzJGG_EGETa0C9A8g=/0x0:3000x2227/1200x800/filters:focal(907x634:1387x1114)/cdn.vox-cdn.com/uploads/chorus_image/image/59377179/944308308.jpg.0.jpg",
-        "scores": [5, 1, 4, 4, 5, 1, 2, 6, 7, 9]
+        "scores": [5, 1, 4, 4, 3, 3, 4, 3, 3, 3]
 
     },
     {
         "name": "Jill",
         "photo": "https://tinder.com/static/build/fcf9fe442c7cfd80da6433af784d59d9.jpg",
-        "scores": [5, 1, 4, 4, 5, 1, 2, 6, 7, 9]
+        "scores": [5, 1, 2, 2, 5, 1, 2, 5, 2, 1]
     },
-    
+
 ]
 
 module.exports = friends;
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -7,7 +7,7 @@ module.exports = function (app) {
     })
 
     //any wierd routes get sent to the homepage
-    app.get("*", function(req, res) {
+    app.get("*", function (req, res) {
         res.sendFile(path.join(__dirname, "../public/home.html"))
     })
 
