13024b3d9c43d0437e0ab7b1ea985844ecb53127
Tad <tad.3hatsmedia@gmail.com>
2018-08-30 22:04:15 -0500

    update api/friends list

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,35 +5,34 @@ module.exports = function (app) {
 
     //all possible friends
     app.get("/api/friends", function (req, res) {
+        res.json(friends);
+        // console.log(friends);
+    })
+        ;
+
+    //Handle incoming survey results and handle survey compatibility logic     
+    app.post("/api/friends", function (req, res) {
+        console.log(req.body);
+        var lowestScore = 100;
+        var match;
+        var clientScore = req.body.scores.reduce((a, b) => parseInt(a) + parseInt(b), 0)
+
         for (var i = 0; i < friends.length; i++) {
-            res.json(friends[i].photo);
-            // console.log(friends);
+            var friendScore = friends[i].scores.reduce((a, b) => parseInt(a) + parseInt(b), 0)
+            var difference = Math.abs(clientScore - friendScore);
+
+            if (difference < lowestScore) {
+                lowestScore = difference;
+                match = friends[i];
+            }
         }
-});
 
-//Handle incoming survey results and handle survey compatibility logic     
-app.post("/api/friends", function (req, res) {
-    console.log(req.body);
-    var lowestScore = 100;
-    var match;
-    var clientScore = req.body.scores.reduce((a, b) => parseInt(a) + parseInt(b), 0)
+        console.log(match);
 
-    for (var i = 0; i < friends.length; i++) {
-        var friendScore = friends[i].scores.reduce((a, b) => parseInt(a) + parseInt(b), 0)
-        var difference = Math.abs(clientScore - friendScore);
-
-        if (difference < lowestScore) {
-            lowestScore = difference;
-            match = friends[i];
-        }
-    }
-
-    console.log(match);
-
-    res.json(match);
+        res.json(match);
 
 
 
-})
+    })
 
 }
